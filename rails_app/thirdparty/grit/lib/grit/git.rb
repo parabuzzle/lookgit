3c6318ec7c48adb98342cfe18d62375cca0c2205
Mike <parabuzzle@gmail.com>
2008-09-10 13:51:53 -0700

    Thirdpart files added

---
+++ rails_app/thirdparty/grit/lib/grit/git.rb
@@ -0,0 +1,116 @@
+trap("CHLD") do
+  begin
+    Process.wait(-1, Process::WNOHANG)
+  rescue Object
+  end
+end
+
+module Grit
+  
+  class Git
+    class GitTimeout < RuntimeError
+      attr_reader :command, :bytes_read
+
+      def initialize(command = nil, bytes_read = nil)
+        @command = command
+        @bytes_read = bytes_read
+      end
+    end
+
+    undef_method :clone
+    
+    class << self
+      attr_accessor :git_binary, :git_timeout
+    end
+  
+    self.git_binary  = "/usr/bin/env git"
+    self.git_timeout = 5
+    
+    attr_accessor :git_dir, :bytes_read
+    
+    def initialize(git_dir)
+      self.git_dir    = git_dir
+      self.bytes_read = 0
+    end
+    
+    # Run the given git command with the specified arguments and return
+    # the result as a String
+    #   +cmd+ is the command
+    #   +options+ is a hash of Ruby style options
+    #   +args+ is the list of arguments (to be joined by spaces)
+    #
+    # Examples
+    #   git.rev_list({:max_count => 10, :header => true}, "master")
+    #
+    # Returns String
+    def method_missing(cmd, options = {}, *args)
+      run('', cmd, '', options, args)
+    end
+    
+    def run(prefix, cmd, postfix, options, args)
+      timeout  = options.delete(:timeout)
+      timeout  = true if timeout.nil?
+
+      opt_args = transform_options(options)
+      ext_args = args.map { |a| a == '--' ? a : "'#{a}'" }
+      
+      call = "#{prefix}#{Git.git_binary} --git-dir='#{self.git_dir}' #{cmd.to_s.gsub(/_/, '-')} #{(opt_args + ext_args).join(' ')}#{postfix}"
+      puts call if Grit.debug
+      response = timeout ? sh(call) : wild_sh(call)
+      puts response if Grit.debug
+      response
+    end
+
+    def sh(command)
+      pid, _, io, _ = Open4.popen4(command)
+      ret = Timeout.timeout(self.class.git_timeout) { io.read }
+      @bytes_read += ret.size
+
+      if @bytes_read > 5242880 # 5.megabytes
+        bytes = @bytes_read
+        @bytes_read = 0
+        raise GitTimeout.new(command, bytes) 
+      end
+
+      ret
+    rescue Object => e
+      Process.kill('KILL', pid) rescue nil
+      bytes = @bytes_read
+      @bytes_read = 0
+      raise GitTimeout.new(command, bytes)
+    end
+
+    def wild_sh(command)
+      pid, _, io, _ = Open4.popen4(command)
+      io.read
+    end
+
+    # Transform Ruby style options into git command line options
+    #   +options+ is a hash of Ruby style options
+    #
+    # Returns String[]
+    #   e.g. ["--max-count=10", "--header"]
+    def transform_options(options)
+      args = []
+      options.keys.each do |opt|
+        if opt.to_s.size == 1
+          if options[opt] == true
+            args << "-#{opt}"
+          else
+            val = options.delete(opt)
+            args << "-#{opt.to_s} '#{val}'"
+          end
+        else
+          if options[opt] == true
+            args << "--#{opt.to_s.gsub(/_/, '-')}"
+          else
+            val = options.delete(opt)
+            args << "--#{opt.to_s.gsub(/_/, '-')}='#{val}'"
+          end
+        end
+      end
+      args
+    end
+  end # Git
+  
+end # Grit
---
+++ rails_app/thirdparty/grit/test/test_git.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + '/helper'
+
+class TestGit < Test::Unit::TestCase
+  def setup
+    @git = Git.new(File.join(File.dirname(__FILE__), *%w[..]))
+  end
+  
+  def test_method_missing
+    assert_match(/^git version [\w\.]*$/, @git.version)
+  end
+  
+  def test_transform_options
+    assert_equal ["-s"], @git.transform_options({:s => true})
+    assert_equal ["-s '5'"], @git.transform_options({:s => 5})
+    
+    assert_equal ["--max-count"], @git.transform_options({:max_count => true})
+    assert_equal ["--max-count='5'"], @git.transform_options({:max_count => 5})
+    
+    assert_equal ["-s", "-t"], @git.transform_options({:s => true, :t => true}).sort
+  end
+
+  def test_uses_custom_sh_method
+    @git.expects(:sh)
+    @git.something
+  end
+
+  def test_can_skip_timeout
+    @git.expects(:wild_sh)
+    @git.something(:timeout => false)
+  end
+
+  def test_raises_if_too_many_bytes
+    @git.instance_variable_set(:@bytes_read, 6000000)
+    assert_raises Grit::Git::GitTimeout do
+      @git.something
+    end
+  end
+
+  def test_raises_on_slow_shell
+    Grit::Git.git_timeout = 0.5
+    Open4.expects(:popen4).returns([ nil, nil, mock(:read => proc { sleep 1 }), nil ])
+    assert_raises Grit::Git::GitTimeout do
+      @git.something
+    end
+  end
+
+  def test_works_fine_if_quick
+    output = 'output'
+    Open4.expects(:popen4).returns([ nil, nil, mock(:read => output), nil ])
+    assert_equal output, @git.something
+  end
+end
