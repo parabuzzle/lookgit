c335d975deae91079b7becd0bfafd598b3fed73d
Michael Heijmans <heijmans@grewweight.corp.yahoo.com>
2008-12-09 16:06:14 -0800

    changed styling on code files

--- rails_app/app/controllers/application.rb
+++ rails_app/app/controllers/application.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'ebaa3b5c2911fb051ef50d1f3e103b5f'
   Syntaxi::line_number_method = 'none'
+  Syntaxi::wrap_enabled = true
+  Syntaxi::wrap_at_column = 150
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
